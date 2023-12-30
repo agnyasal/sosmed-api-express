5ad2260be077a0a077c4f918f43c190d0e168cfc
agnyasal <geasalsabila21@gmail.com>
2023-12-30 21:39:40 +0700

    feat : add include in get tweets

--- src/repositories/tweets/getAllTweets.ts
+++ src/repositories/tweets/getAllTweets.ts
@@ -2,7 +2,11 @@ import { prisma } from "../../helper/prisma";
 
 export const getAllTweets = async () => {
   try {
-    const allTweets = await prisma.tweets.findMany();
+    const allTweets = await prisma.tweets.findMany({
+      include: {
+        user: true,
+      },
+    });
     return allTweets;
   } catch (error) {
     console.log(error);
